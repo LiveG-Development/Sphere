9a9d562caa202caef8df5e504ca6bcd493b71656
James-Livesey <jamesliveg@gmail.com>
2019-10-01 08:58:48 +0100

    Add documentation to focussing function

--- sphere/funcs/focussed/script.js
+++ sphere/funcs/focussed/script.js
@@ -8,8 +8,15 @@
 // Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 
 var focussed = {
-    inputTypes: [],
+    inputTypes: ["input", "select", "textarea"],
 
+    /*
+        @name focussed.isInputFocussed
+        
+        @return boolean Whether an input is focussed.
+
+        @shortDescription Check if an input is currently focussed.
+    */
     isInputFocussed: function() {
         return document.activeElement && focussed.inputTypes.indexOf(document.activeElement.tagName.toLowerCase()) != -1;
     }
