3318c5c8a807aa363204890808cc03d61f506a41
James Livesey <jamesliveg@gmail.com>
2020-01-28 18:47:18 +0000

    Add setup for building binaries

---
+++ dist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sphere
+# 
+# Copyright (C) LiveG. All Rights Reserved.
+# Copying is not a victimless crime. Anyone caught copying LiveG software may
+# face sanctions.
+# 
+# https://liveg.tech
+# Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+
+cd sphere
+zapr build app
+zapr docgen
+
+cd ..
+
+npm install
+npm run dist
---
+++ res/installer.nsh
@@ -0,0 +1,4 @@
+!macro customHeader
+    !define /redef MUI_ICON "${BUILD_RESOURCES_DIR}/icon.ico"
+    !define /redef MUI_UNICON "${BUILD_RESOURCES_DIR}/icon.ico"
+!macroend
